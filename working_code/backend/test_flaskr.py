93472c76fdd63d22ce248990f38642c2daa21b72
Glitched-Manual <popcorneater720@gmail.com>
2021-10-09 23:38:19 -0400

    starting tests

--- working_code/backend/test_flaskr.py
+++ working_code/backend/test_flaskr.py
@@ -34,6 +34,57 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    """
+    / category test
+    """
+
+    def test_get_categories(self):
+
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        self.assertTrue(data['categories'], True)
+
+        """ 
+       'success': True,
+        'categories': category_dictionary
+        """
+
+    """
+    /questions tests
+    
+    """
+        
+    def test_get_paginated_questions(self):
+        """Test to get some question from the database"""
+
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        # check status code and success message
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+        self.assertTrue(len(data['categories']))
+
+    """
+    Test for page of questions that does not exist
+    """
+    def test_404_page_not_found_request(self):
+
+        res = self.client().get('/questions?page=9000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
