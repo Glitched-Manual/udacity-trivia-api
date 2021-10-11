9e8bd82d962a6053cd6ad59a7f223d6df8cd8368
Glitched-Manual <popcorneater720@gmail.com>
2021-10-11 03:27:05 -0400

    test code was written

--- working_code/backend/test_flaskr.py
+++ working_code/backend/test_flaskr.py
@@ -15,7 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        #self.password = ""
+        #self.database_path = "postgresql://student:student@{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -30,7 +32,7 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
+    
     Write at least one test for each test for successful operation and for expected errors.
     """
 
@@ -183,10 +185,62 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "resource not found")
 
-
+    """
+    /categories/<int:category_id>/questions
     
+    """
+    
+    def test_get_category_questions(self):
+        #get questions from an existing
+        res = self.client().get('/categories/2/questions')
 
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        """
+        'questions': [question.format() for question in questions],
+        'total_questions': len(questions)
+            
+        """
+        self.assertTrue(len(data['questions']), True)
+        self.assertTrue(data['total_questions'], True)
+
+    def test_404_none_existing_category(self):
+
+        res = self.client().get('/categories/9000/questions')
+        data = json.loads(res.data)
         
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], "resource not found")
+
+    def test_play_quiz(self):
+        test_quiz_round = {'previous_questions': [],
+                            'quiz_category': { 'type': 'Art', 'id': 2}}
+
+        res = self.client().post('/quizzes', json=test_quiz_round)
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        
+        self.assertTrue(data['question']['category'], 2)
+        """
+        category = body.get('quiz_category')
+        previous_questions =  body.get('previous_questions')
+        2 | Art
+
+        new_quiz_round = {'previous_questions': [],
+                          'quiz_category': {'type': 'Entertainment', 'id': 5}}
+        """
+    def test_404_quiz_error(self):
+        test_quiz_round = {'previous_questions', []}
+        res = self.client().post('/quizzes', json=test_quiz_round)
+
+        data = json.loads(res.data)
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
